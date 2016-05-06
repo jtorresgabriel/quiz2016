489cbcbc0fdfb423eef8024825a1515647d6ea9d
jtorresgabriel <jorge.torres.degabriel@alumnos.upm.es>
2016-05-06 20:23:58 +0200

    despliegue en heroku bd

--- models/index.js
+++ models/index.js
@@ -4,8 +4,19 @@ var path = require ('path');
 var Sequelize = require ('sequelize');
 
 //usar BBDD SQLite:
-var sequelize = new Sequelize(null, null, null, 
-					{ dialect: "sqlite", storage: "quiz.sqlite"});
+// DATABASE_URL = sqlite:///
+// DATABASE_STORAGE = quiz.sqlite
+
+var url, storage;
+
+if(!process.env.DATABASE_URL){
+	url = "sqlite:///";
+	storage = "quiz.sqlite";
+}else {
+	url = process.env.DATABASE_URL;
+	storage = process.env.DATABASE_STORAGE || "";
+}
+ var sequelize = new Sequelize (url, { storage: storage, omit: true});
 
 // Importar la definición de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
