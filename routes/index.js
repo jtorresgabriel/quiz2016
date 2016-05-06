b70c3d9525c7e18475cbe1df73a7019103201717
jtorresgabriel <jorge.torres.degabriel@alumnos.upm.es>
2016-05-06 02:58:52 +0200

    autores y css adaptativo

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,15 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller'); 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz2016' });
+  res.render('index');
+});
+router.get('/autor', function(req, res) {
+  res.render('autor');
 });
 
 
 router.get('/question', quizController.question);
 router.get('/check', quizController.check);
+
 
 module.exports = router;
